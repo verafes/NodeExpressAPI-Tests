71284e6196551d129668ee155e41122dade6b1c6
verafes <verafes@gmail.com>
2024-10-25 23:06:20 -0700

    Add API tests for GET users endpoint

--- tests/apiTest.spec.js
+++ tests/apiTest.spec.js
@@ -12,6 +12,7 @@ const userSecond = {
     "lastName": "Ivanov",
     "age": 25
 }
+let userId = '5f7abd01-4047-4b0a-8b79-d82037dd02c9'
 
 test('GET /', async() => {
     const apiRequest = await request.newContext()
@@ -35,4 +36,34 @@ test('GET /', async() => {
     expect(await response.text()).toEqual("Node Express API Server")
     expect(statusCode).toBe(200)
     await expect(response).toBeOK()
+})
+
+test('GET list of the users', async() => {
+    const apiRequest = await request.newContext()
+    const response = await apiRequest.get(`${BASE_URL}/users`)
+    await expect(response.status()).toBe(200)
+    await expect(await response.text()).toEqual("There are no users.")
+})
+
+test('Create users', async () => {
+    const apiRequest = await request.newContext()
+    const response = await apiRequest.post(`${BASE_URL}/users`,{
+        data: userFirst
+    })
+    await expect(response.status()).toBe(200)
+    await expect(await response.text()).toEqual("User created successfully.")
+})
+
+test('Get users id', async() => {
+    const apiRequest = await request.newContext()
+    const response = await apiRequest.get(`${BASE_URL}/users`)
+    // console.log("userId = " + response.json().id)
+    const headersArray = response.headersArray()
+    const contentType = headersArray
+        .find((header) => header.name === 'Content-Type')
+        .value
+    console.log("contentType = " + contentType)
+    // const createdUser = await response.body()
+    userId = await response.body()
+    console.log("id = " + userId[0])
 })
