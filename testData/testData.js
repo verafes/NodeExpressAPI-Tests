8a69d691b70d7c94112176c52a8055b09146d797
verafes <verafes@gmail.com>
2024-10-27 16:11:34 -0700

    Add test data, functional test; set headless mode to false

---
+++ testData/testData.js
@@ -0,0 +1,23 @@
+export const BASE_URL = 'http://localhost:5000';
+
+export const expectedHeaders = {
+    title: 'Users App',
+    h1: 'Node Express API Server App',
+    addUserH2: 'Add User',
+    userListH2: 'Users List',
+}
+export const expectedH2 = [ 'Add User', 'Users List' ]
+
+export const expectedFormTexts = {
+    firstNameLabel: 'First Name',
+    lastNameLabel: 'Last Name',
+    age: 'Age',
+    firstNameInputPlaceholder: 'Enter first name ...',
+    lastNameInputPlaceholder: 'Enter first name ...',
+    geInputPlaceholder: 'Enter first name ...',
+    addButton: 'Add',
+}
+
+export const expectedTablesHeadList = [
+    '#', 'First', 'Last', 'Age', 'ID'
+]
--- tests/uiTest.spec.js
+++ tests/uiTest.spec.js
@@ -1,24 +1,56 @@
 import { test, expect } from '@playwright/test';
+import * as testData from '../testData/testData.js';
 
-const BASE_URL = 'http://localhost:5000';
 
-test('Title and URL as expected', async ({ page }) => {
-    await page.goto(`${BASE_URL}/`);
+test('Title and URL as expected - Smoke test', async ({ page }) => {
+    await page.goto(`${testData.BASE_URL}/`);
 
-    await expect(page).toHaveURL("http://localhost:5000/");
-    await expect(page).toHaveTitle("My Express App");
+    await expect(page).toHaveURL(`${testData.BASE_URL}/`);
+    await expect(page).toHaveTitle(testData.expectedHeaders.title);
 });
 
-test('Header text', async ({ page }) => {
-    const expectedHeader = 'Node Express API Server App';
-    await page.goto(`${BASE_URL}/`);
+test('H1 heading display and text validation - UI test', async ({ page }) => {
+    await page.goto(`${testData.BASE_URL}/`);
 
-    const headerLocator = page.getByRole('heading', { name: expectedHeader });
+    const headerLocator = page.getByRole('heading', { name: testData.expectedHeaders.h1 });
 
     await expect(headerLocator).toBeVisible();
 
     //second version
-    const headerCssLocator = page.locator("#appName");
+    const headerCssLocator = page.locator('#appName');
     
-    await expect(headerCssLocator).toHaveText(expectedHeader);
+    await expect(headerCssLocator).toHaveText(testData.expectedHeaders.h1);
 });
+
+test('H2 headings display and text validation - UI test', async ({ page }) => {
+    await page.goto(`${testData.BASE_URL}/`);
+
+    const allH2 = await page.locator('h2');
+    const h2Count = await allH2.count();
+
+    await expect(h2Count).toBe(2);
+    for (let i = 0; i < h2Count; i++) {
+        await expect(allH2.nth(i)).toBeVisible();
+        await expect(allH2.nth(i)).toHaveText(testData.expectedH2[i]);
+    }
+});
+
+test('User form submission - Functional test', async ({ page }) => {
+    await page.goto(`${testData.BASE_URL}/`);
+
+    const firstNamePlaceholder = page.getByPlaceholder('Enter first name ...', { exact: true });
+    await firstNamePlaceholder.fill("John");
+
+    const lastNameLabel =  await page.getByLabel('Last Name', { exact: true });
+    await lastNameLabel.fill("Doe");
+
+    const ageId = await page.getByTestId('age');
+    await ageId.fill('34');
+
+    await expect(firstNamePlaceholder).toHaveValue('John');
+    await expect(lastNameLabel).toHaveValue('Doe');
+    await expect(ageId).toHaveValue('34');
+
+    const addButton = await page.getByRole('button', { name: 'Add', exact: true });
+    await addButton.click();
+})
